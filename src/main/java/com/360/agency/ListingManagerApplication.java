e41bf3a593b6a08c9fc587c7053327f8d41c3f66
Mr-abdoulraouf <olpad008@gmail.com>
2023-06-06 10:44:56 +0100

    init listing manager project

---
+++ src/main/java/com/360/agency/ListingManagerApplication.java
@@ -0,0 +1,13 @@
+package com0.agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/360/agency/ListingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com0.agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
