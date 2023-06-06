2c2e5cc1da026f1276b5d31b6fcbc92fdceea444
Mr-abdoulraouf <olpad008@gmail.com>
2023-06-06 12:55:12 +0100

    Package service classes annotated and dao classes initialized

--- src/main/java/com/agency360/listing/service/ListingService.java
+++ src/main/java/com/agency360/listing/service/ListingService.java
@@ -1,5 +1,7 @@
 package com.agency360.listing.service;
 
+import org.springframework.stereotype.Service;
+@Service
 public interface ListingService {
 
 
--- src/main/java/com/agency360/listing/service/ListingserviceImp.java
+++ src/main/java/com/agency360/listing/service/ListingserviceImp.java
@@ -1,4 +1,17 @@
 package com.agency360.listing.service;
 
+import com.agency360.listing.model.tables.daos.DealerDao;
+import com.agency360.listing.model.tables.daos.ListingDao;
+import com.agency360.listing.model.tables.daos.TierLimitDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ListingserviceImp implements ListingService{
+
+    private final ListingDao listingDao;
+    private final DealerDao dealerDao;
+    private final TierLimitDao tierLimitDao;
+
 }
