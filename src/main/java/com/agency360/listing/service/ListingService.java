0579908079ed635c0a25f5f8bdbbac5b3a9c2bb9
Mr-abdoulraouf <olpad008@gmail.com>
2023-06-06 17:26:07 +0100

    listing service functionalities

--- src/main/java/com/agency360/listing/service/ListingService.java
+++ src/main/java/com/agency360/listing/service/ListingService.java
@@ -1,8 +1,20 @@
 package com.agency360.listing.service;
 
+
+import com.agency360.listing.model.tables.pojos.Listing;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
 @Service
 public interface ListingService {
 
 
+    void create (Listing listing);
+    void update(Listing listing);
+    Set<Listing> getListingByDealerIdAndState(Integer dealerId, String state);
+
+    void publishListing(Integer id);
+
+    void unpublishListing(Integer id);
 }
--- src/main/java/com/agency360/listing/service/ListingserviceImp.java
+++ src/main/java/com/agency360/listing/service/ListingserviceImp.java
@@ -1,17 +1,51 @@
 package com.agency360.listing.service;
 
+
+
 import com.agency360.listing.model.tables.daos.DealerDao;
 import com.agency360.listing.model.tables.daos.ListingDao;
 import com.agency360.listing.model.tables.daos.TierLimitDao;
+import com.agency360.listing.model.tables.pojos.Listing;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class ListingserviceImp implements ListingService{
 
     private final ListingDao listingDao;
-    private final DealerDao dealerDao;
-    private final TierLimitDao tierLimitDao;
 
+    Logger logger = LoggerFactory.getLogger(ListingserviceImp.class);
+    @Override
+    public void create(Listing listing) {
+
+
+    }
+
+    @Override
+    public void update(Listing listing) {
+
+    }
+
+    @Override
+    public Set<Listing> getListingByDealerIdAndState(Integer dealerId, String state) {
+        return null;
+    }
+
+    @Override
+    public void publishListing(Integer id) {
+
+    }
+
+    @Override
+    public void unpublishListing(Integer id) {
+
+    }
 }
